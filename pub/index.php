f7c4a08c9ca28fe83cabc025573d980bf2f62ae8
lafnlab <barista@gottahavacuppamocha.com>
2018-12-06 11:34:29 -0500

    Initial PHP pages in pub

---
+++ pub/index.php
@@ -0,0 +1,23 @@
+<?php
+include_once "../conn.php";
+$pagetitle = "🖤";
+
+include_once "main-header.php";
+?>
+
+		<ul>
+			<li><a href="list-places.php"><?php echo _("List of places"); ?></a></li>
+			<li><a href="list-nationalities.php"><?php echo _("List of nationalities"); ?></a></li>
+			<li><a href="list-time-zones.php"><?php echo _("List of time zones"); ?></a></li>
+			<li><a href="list-currencies.php"><?php echo _("List of currencies"); ?></a></li>
+			<li><a href="list-spoken-languages.php"><?php echo _("List of languages"); ?></a></li>
+			<li><a href="list-locales.php"><?php echo _("List of locales"); ?></a></li>
+			<li><a href="list-genders.php"><?php echo _("List of genders"); ?></a></li>
+			<li><a href="list-sexualities.php"><?php echo _("List of sexualities"); ?></a></li>
+			<li><a href="list-hair-colors.php"><?php echo _("List of hair colors"); ?></a></li>
+			<li><a href="list-eye-colors.php"><?php echo _("List of eye colors"); ?></a></li>
+		</ul>
+
+<?php
+include_once "main-footer.php";
+?>
---
+++ pub/main-header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title><?php echo _($pagetitle); ?></title>
+	<link href="style/blackheart.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<main>
