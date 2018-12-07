4502c40f982b7eeaed113fb6bbba0c266c856ce2
lafnlab <barista@gottahavacuppamocha.com>
2018-12-07 09:57:27 -0500

    Removed non-functioning url

--- pub/index.php
+++ pub/index.php
@@ -6,16 +6,16 @@ include_once "main-header.php";
 ?>
 
 		<ul>
-			<li><a href="list-currencies.php"><?php echo _("List of social.media.dating currencies"); ?></a></li>
-			<li><a href="list-eye-colors.php"><?php echo _("List of social.media.dating eye colors"); ?></a></li>
-			<li><a href="list-genders.php"><?php echo _("List of social.media.dating genders"); ?></a></li>
-			<li><a href="list-hair-colors.php"><?php echo _("List of social.media.dating hair colors"); ?></a></li>
-			<li><a href="list-locales.php"><?php echo _("List of social.media.dating locales"); ?></a></li>
-			<li><a href="list-nationalities.php"><?php echo _("List of social.media.dating nationalities"); ?></a></li>
-			<li><a href="list-places.php"><?php echo _("List of social.media.dating places"); ?></a></li>
-			<li><a href="list-sexualities.php"><?php echo _("List of social.media.dating sexualities"); ?></a></li>
-			<li><a href="list-spoken-languages.php"><?php echo _("List of social.media.dating spoken languages"); ?></a></li>
-			<li><a href="list-time-zones.php"><?php echo _("List of social.media.dating time zones"); ?></a></li>
+			<li><a href="list-currencies.php"><?php echo _("List of currencies"); ?></a></li>
+			<li><a href="list-eye-colors.php"><?php echo _("List of eye colors"); ?></a></li>
+			<li><a href="list-genders.php"><?php echo _("List of genders"); ?></a></li>
+			<li><a href="list-hair-colors.php"><?php echo _("List of hair colors"); ?></a></li>
+			<li><a href="list-locales.php"><?php echo _("List of locales"); ?></a></li>
+			<li><a href="list-nationalities.php"><?php echo _("List of nationalities"); ?></a></li>
+			<li><a href="list-places.php"><?php echo _("List of places"); ?></a></li>
+			<li><a href="list-sexualities.php"><?php echo _("List of sexualities"); ?></a></li>
+			<li><a href="list-spoken-languages.php"><?php echo _("List of spoken languages"); ?></a></li>
+			<li><a href="list-time-zones.php"><?php echo _("List of time zones"); ?></a></li>
 		</ul>
 
 <?php
