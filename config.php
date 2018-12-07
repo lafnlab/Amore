0ea937b90c30bb5d5aea6b6a2ea2092b26fd4bd7
lafnlab <barista@gottahavacuppamocha.com>
2018-12-07 14:04:35 -0500

    created config file

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+#
+# $sitetitle is the title of the whole site.
+# 'Amore' is default, but you should change it to the name of your site.
+# The site title will appear when the page title is unavailable.
+$sitetitle = "Amore";
+
+#
+# $greeting is a generic greeting for any visitor to the site.
+# If a user is logged in, the greeting will change to one appropriate for them.
+# 'Hello' is the default, but can be changed to whatever you prefer.
+$greeting = "Hello";
+
+#
+# $visitortitle is a generic term for a visitor who isn't logged in.
+# 'Guest' is default, but can be changed to anything.
+$visitortitle = "Guest";
+?>
