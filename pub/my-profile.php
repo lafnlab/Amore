1ebd6204db7c737c9fb76921cf49aea176f2b52f
lafnlab <barista@gottahavacuppamocha.com>
2018-12-11 10:57:27 -0500

    Sloppy with the cut and paste. Now fixed.

--- pub/my-profile.php
+++ pub/my-profile.php
@@ -29,16 +29,16 @@ include_once "main-header.php";
 ?>
 
 		<ul>
-			<li><a href="list-currencies.php"><?php echo _("List of social.media.dating currencies"); ?></a></li>
-			<li><a href="list-eye-colors.php"><?php echo _("List of social.media.dating eye colors"); ?></a></li>
-			<li><a href="list-genders.php"><?php echo _("List of social.media.dating genders"); ?></a></li>
-			<li><a href="list-hair-colors.php"><?php echo _("List of social.media.dating hair colors"); ?></a></li>
-			<li><a href="list-locales.php"><?php echo _("List of social.media.dating locales"); ?></a></li>
-			<li><a href="list-nationalities.php"><?php echo _("List of social.media.dating nationalities"); ?></a></li>
-			<li><a href="list-places.php"><?php echo _("List of social.media.dating places"); ?></a></li>
-			<li><a href="list-sexualities.php"><?php echo _("List of social.media.dating sexualities"); ?></a></li>
-			<li><a href="list-spoken-languages.php"><?php echo _("List of social.media.dating spoken languages"); ?></a></li>
-			<li><a href="list-time-zones.php"><?php echo _("List of social.media.dating time zones"); ?></a></li>
+			<li><a href="list-currencies.php"><?php echo _("List of $sitetitle currencies"); ?></a></li>
+			<li><a href="list-eye-colors.php"><?php echo _("List of $sitetitle eye colors"); ?></a></li>
+			<li><a href="list-genders.php"><?php echo _("List of $sitetitle genders"); ?></a></li>
+			<li><a href="list-hair-colors.php"><?php echo _("List of $sitetitle hair colors"); ?></a></li>
+			<li><a href="list-locales.php"><?php echo _("List of $sitetitle locales"); ?></a></li>
+			<li><a href="list-nationalities.php"><?php echo _("List of $sitetitle nationalities"); ?></a></li>
+			<li><a href="list-places.php"><?php echo _("List of $sitetitle places"); ?></a></li>
+			<li><a href="list-sexualities.php"><?php echo _("List of $sitetitle sexualities"); ?></a></li>
+			<li><a href="list-spoken-languages.php"><?php echo _("List of $sitetitle spoken languages"); ?></a></li>
+			<li><a href="list-time-zones.php"><?php echo _("List of $sitetitle time zones"); ?></a></li>
 		</ul>
 
 <?php
