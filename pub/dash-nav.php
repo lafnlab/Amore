e30d13cb14a3cc6b8415c6caccde20c021b874e3
lafnlab <barista@gottahavacuppamocha.com>
2018-12-20 10:12:39 -0500

    Oops. I guess alt doesn't work on <a>. Switched to title instead.

--- pub/dash-nav.php
+++ pub/dash-nav.php
@@ -1,13 +1,13 @@
 <nav class="dashnav">
   <ul>
-    <li><a href="#" alt="Home will show posts from everyone the user is following, including their own.">Home</a></li>
-    <li><a href="#" alt="Mentions will show any posts where the user is mentioned.">Mentions</a></li>
-    <li><a href="#" alt="Messages will be private/direct messages between friends.">Messages</a></li>
-    <li><a href="#" alt="A list of the user's friends (Friends follow each other).">Friends</a></li>
-    <li><a href="#" alt="A list of accounts the user follows.">Following</a></li>
-    <li><a href="#" alt="A list of accounts that follow this user.">Followers</a></li>
-    <li><a href="#" alt="Lists created by the user">Lists</a></li>
-    <li><a href="#" alt="A list of the user's favorites">Favorites</a></li>
+    <li><a href="#" title="Home will show posts from everyone the user is following, including their own.">Home</a></li>
+    <li><a href="#" title="Mentions will show any posts where the user is mentioned.">Mentions</a></li>
+    <li><a href="#" title="Messages will be private/direct messages between friends.">Messages</a></li>
+    <li><a href="#" title="A list of the user's friends (Friends follow each other).">Friends</a></li>
+    <li><a href="#" title="A list of accounts the user follows.">Following</a></li>
+    <li><a href="#" title="A list of accounts that follow this user.">Followers</a></li>
+    <li><a href="#" title="Lists created by the user">Lists</a></li>
+    <li><a href="#" title="A list of the user's favorites">Favorites</a></li>
   </ul>
 </nav>
 <main>
