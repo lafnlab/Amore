1f2a904041ef82353135448089466339e9dcb86f
lafnlab <barista@gottahavacuppamocha.com>
2018-12-24 11:05:50 -0500

    Added code for showing posts.

--- pub/my-profile.php
+++ pub/my-profile.php
@@ -48,6 +48,42 @@ include_once "dash-nav.php";
 				<input type="radio" class="addpostradio" name="addpostradio" value="ñToùòхаþOЪ" ><?php echo _("SELF"); ?>&nbsp;&nbsp;
 				<input type="submit" id="addpostsubmit" name="addpostsubmit" value="<?php echo _('Post'); ?>">
 			</form>
+<?php
+// let's see if there are any posts to view
+$pst_q = "SELECT * FROM pst WHERE pst_priv=\"6ьötХ5áзÚZ\"";
+$pst_query = mysqli_query($dbconn,$pst_q);
+if (mysqli_num_rows($pst_query) <> 0) {
+	while ($pst_opt = mysqli_fetch_assoc($pst_query)) {
+		$postid		= $pst_opt['pst_id'];
+		$postby		= $pst_opt['pst_by'];
+		$posttime	= $pst_opt['pst_timestamp'];
+		$posttext	= $pst_opt['pst_text'];
+		$postlang	= $pst_opt['pst_lang'];
+		$postpriv	= $pst_opt['pst_priv'];
+
+		$by_q = "SELECT * FROM usr WHERE usr_id=\"".$postby."\"";
+		$by_query = mysqli_query($dbconn,$by_q);
+		while($by_opt = mysqli_fetch_assoc($by_query)) {
+			$byname		= $by_opt['usr_name'];
+		}
+
+
+		echo "\t\t\t<div class=\"showpost\">\n";
+		echo "\t\t\t\t<span class=\"showpostby\">".$byname;
+		echo "<a href=\"post.php?pid=".$postid."\">".timediff($posttime);
+		echo "</a></span>\n";
+		echo "\t\t\t\t<p class=\"showposttext\">".$posttext."</p>\n";
+		echo "\t\t\t\t<!-- future functionality on span below -->\n";
+		echo "\t\t\t\t<a href=\"#\" title=\"Reply\">⮪0</a>&nbsp;<a href=\"#\" title=\"Upvote\">⤊0</a>&nbsp;<a href=\"#\" title=\"Downvote\">⤋0</a>&nbsp;<a href=\"#\" title=\"Favorite\">🎔 0</a>&nbsp;…";
+		echo "\t\t\t</div>\n";
+	}
+} else {
+		echo "\t\t\t<div class=\"showpost\">\n";
+		echo _("There are no posts at the moment");
+		echo $pst_q;
+		echo "\t\t\t</div>\n";
+}
+?>
 		</article>
 <?php
 include_once "dash-footer.php";
