d4a43c3f208262d568fa6d2e074726a6ec2aac77
lafnlab <barista@gottahavacuppamocha.com>
2018-12-28 11:35:22 -0500

    Update add-nationality.php

--- pub/add-nationality.php
+++ pub/add-nationality.php
@@ -6,7 +6,7 @@ include			"../functions.php";
 $dbconn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
 mysqli_set_charset($dbconn, "utf8");
 
-$pagetitle 	= "ADD NATIONALITY TITLE";
+$pagetitle 	= "Add a nationality";
 #$message		= 'test message';
 
 // PROCESSING
