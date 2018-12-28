fede7c016efba483fca3f311bb8eb8e02f48763c
lafnlab <barista@gottahavacuppamocha.com>
2018-12-28 11:52:18 -0500

    Update list-sexualities.php

--- pub/list-sexualities.php
+++ pub/list-sexualities.php
@@ -6,7 +6,8 @@ include			"../functions.php";
 $dbconn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
 mysqli_set_charset($dbconn, "utf8");
 
-$pagetitle = "LIST SEXUALITIES TITLE";
+$pagetitle = "List of sexualities";
+
 
 include_once "main-header.php";
 ?>
