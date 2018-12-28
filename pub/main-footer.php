71b1210758fb738f9333a7d04024983fb619c353
lafnlab <barista@gottahavacuppamocha.com>
2018-12-28 11:56:21 -0500

    Update main-footer.php

--- pub/main-footer.php
+++ pub/main-footer.php
@@ -1,4 +1,4 @@
-		<span>Powered by <a href="https://github.com/lafnlab/Amore">Amore</a></span>
+		<span>Powered by <a href="https://github.com/lafnlab/Amore">Amore</a> v0.1</span>
 	</main>
 </body>
 </html>
