ed0f26fd2ee2ae66c12d3f40359bd678ecc98688
lafnlab <barista@gottahavacuppamocha.com>
2018-12-28 11:50:00 -0500

    Update list-hair-colors.php

--- pub/list-hair-colors.php
+++ pub/list-hair-colors.php
@@ -6,7 +6,8 @@ include			"../functions.php";
 $dbconn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
 mysqli_set_charset($dbconn, "utf8");
 
-$pagetitle = "LIST HAIR COLORS TITLE";
+$pagetitle = "List of hair colors";
+
 
 include_once "main-header.php";
 ?>
