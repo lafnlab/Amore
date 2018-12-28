00f43ee5679d632c6de372d80e356568cceae7e7
lafnlab <barista@gottahavacuppamocha.com>
2018-12-28 11:35:57 -0500

    Update add-place.php

--- pub/add-place.php
+++ pub/add-place.php
@@ -6,7 +6,7 @@ include			"../functions.php";
 $dbconn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
 mysqli_set_charset($dbconn, "utf8");
 
-$pagetitle 	= "ADD PLACE TITLE";
+$pagetitle 	= "Add a place";
 #$message		= 'test message';
 
 // PROCESSING
