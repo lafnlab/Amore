651ae9a3114854962bda47cee47e197014fa4fa0
lafnlab <barista@gottahavacuppamocha.com>
2018-12-28 11:28:01 -0500

    Update add-eye-color.php

--- pub/add-eye-color.php
+++ pub/add-eye-color.php
@@ -6,23 +6,24 @@ include			"../functions.php";
 $dbconn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
 mysqli_set_charset($dbconn, "utf8");
 
-$pagetitle 	= "ADD EYE COLOR TITLE";
+$pagetitle 	= "Add a place to social.media.dating";
 #$message		= 'test message';
 
 // PROCESSING
-if (isset($_POST['eyesubmit'])) {
+if (isset($_POST['placesubmit'])) {
 
-	$eyid			= makeid($newid);
-	$eycolor		= nicetext($_POST['eyecolor']);
+	$plid			= makeid($newid);
+	$plname		= nicetext($_POST['placename']);
+	$plparent	= join(',',$_POST['placeparent']); // allows for multiple options
 
 	// is the id unique in this table?
-	$idq = "SELECT * FROM eye WHERE eye_id=\'".$eyid."\'";
+	$idq = "SELECT * FROM loc WHERE loc_id=\'".$plid."\'";
 	$idquery = mysqli_query($dbconn,$idq);
 	$message = $idq;
 	if ($idquery == FALSE) {
 
-		$eyaddq 	= "INSERT INTO eye (eye_id, eye_color) VALUES ('$eyid','$eycolor')";
-		$eyaddquery	= mysqli_query($dbconn,$eyaddq);
+		$pladdq 	= "INSERT INTO loc (loc_id, loc_name, loc_parent) VALUES ('$plid','$plname','$plparent')";
+		$pladdquery	= mysqli_query($dbconn,$pladdq);
 		$message 	= "Operation complete. Add another section or click <a href=\"/\">here</a> to return to the main page.";
 #		redirect('index.php');
 	} else {
@@ -30,7 +31,7 @@ if (isset($_POST['eyesubmit'])) {
 #		redirect('index.html');
 	}
 
-} // if isset $_POST 'eyesubmit'
+} // if isset $_POST 'placesubmit'
 
 include_once "main-header.php";
 ?>
@@ -44,11 +45,26 @@ if ($message != '' || NULL) {
 				<table>
 					<caption><?php echo _($pagetitle); ?></caption>
 					<tr>
-						<td class="inputlabel"><label for="eyecolor"><?php echo _('Eye color');?></label></td>
-						<td><input type="text" name="eyecolor" id="eyecolor" class="inputtext" required maxlength="100"></td>
+						<td class="inputlabel"><label for="placename"><?php echo _('Place name');?></label></td>
+						<td><input type="text" name="placename" id="placename" class="inputtext" required maxlength="100"></td>
+					</tr>
+					<tr>
+						<td class="inputlabel"><label for="placeparent"><?php echo _('Place parent'); ?></label></td>
+						<!-- this list is an imperfect mess, at best. It will be improved at some point -->
+						<td>
+							<select multiple name="placeparent[]" id="placeparent" class="inputselectmulti">
+<?php
+		$parentq = "SELECT * FROM loc ORDER BY loc_parent,loc_name ASC";
+		$parentquery = mysqli_query($dbconn,$parentq);
+		while ($parentopt = mysqli_fetch_assoc($parentquery)) {
+			echo "\t\t\t\t\t\t\t\t\t<option value=\"".$parentopt['loc_id']."\">".$parentopt['loc_name']."</option>\n";
+		}
+?>
+							</select>
+						</td>
 					</tr>
 				</table>
-				<input type="submit" name="eyesubmit" id="eyesubmit" class="button" value="<?php echo _('Submit'); ?>">
+				<input type="submit" name="placesubmit" id="placesubmit" class="button" value="<?php echo _('Submit'); ?>">
 			</form>
 		</article>
 
