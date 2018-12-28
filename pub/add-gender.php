c002944c4756ba87638e88447b63df7bf7718ea1
lafnlab <barista@gottahavacuppamocha.com>
2018-12-28 11:30:18 -0500

    Whoops. Fixed the update to add-gender.php

--- pub/add-gender.php
+++ pub/add-gender.php
@@ -6,24 +6,23 @@ include			"../functions.php";
 $dbconn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
 mysqli_set_charset($dbconn, "utf8");
 
-$pagetitle 	= "Add a place to social.media.dating";
+$pagetitle 	= "Add a gender";
 #$message		= 'test message';
 
 // PROCESSING
-if (isset($_POST['placesubmit'])) {
+if (isset($_POST['gensubmit'])) {
 
-	$plid			= makeid($newid);
-	$plname		= nicetext($_POST['placename']);
-	$plparent	= join(',',$_POST['placeparent']); // allows for multiple options
+	$gnid			= makeid($newid);
+	$gnname		= nicetext($_POST['genname']);
 
 	// is the id unique in this table?
-	$idq = "SELECT * FROM loc WHERE loc_id=\'".$plid."\'";
+	$idq = "SELECT * FROM gen WHERE gen_id=\'".$gnid."\'";
 	$idquery = mysqli_query($dbconn,$idq);
 	$message = $idq;
 	if ($idquery == FALSE) {
 
-		$pladdq 	= "INSERT INTO loc (loc_id, loc_name, loc_parent) VALUES ('$plid','$plname','$plparent')";
-		$pladdquery	= mysqli_query($dbconn,$pladdq);
+		$geaddq 	= "INSERT INTO gen (gen_id, gen_name) VALUES ('$gnid','$gnname')";
+		$geaddquery	= mysqli_query($dbconn,$geaddq);
 		$message 	= "Operation complete. Add another section or click <a href=\"/\">here</a> to return to the main page.";
 #		redirect('index.php');
 	} else {
@@ -31,7 +30,7 @@ if (isset($_POST['placesubmit'])) {
 #		redirect('index.html');
 	}
 
-} // if isset $_POST 'placesubmit'
+} // if isset $_POST 'gensubmit'
 
 include_once "main-header.php";
 ?>
@@ -45,26 +44,11 @@ if ($message != '' || NULL) {
 				<table>
 					<caption><?php echo _($pagetitle); ?></caption>
 					<tr>
-						<td class="inputlabel"><label for="placename"><?php echo _('Place name');?></label></td>
-						<td><input type="text" name="placename" id="placename" class="inputtext" required maxlength="100"></td>
-					</tr>
-					<tr>
-						<td class="inputlabel"><label for="placeparent"><?php echo _('Place parent'); ?></label></td>
-						<!-- this list is an imperfect mess, at best. It will be improved at some point -->
-						<td>
-							<select multiple name="placeparent[]" id="placeparent" class="inputselectmulti">
-<?php
-		$parentq = "SELECT * FROM loc ORDER BY loc_parent,loc_name ASC";
-		$parentquery = mysqli_query($dbconn,$parentq);
-		while ($parentopt = mysqli_fetch_assoc($parentquery)) {
-			echo "\t\t\t\t\t\t\t\t\t<option value=\"".$parentopt['loc_id']."\">".$parentopt['loc_name']."</option>\n";
-		}
-?>
-							</select>
-						</td>
+						<td class="inputlabel"><label for="genname"><?php echo _('Gender name');?></label></td>
+						<td><input type="text" name="genname" id="genname" class="inputtext" required maxlength="100"></td>
 					</tr>
 				</table>
-				<input type="submit" name="placesubmit" id="placesubmit" class="button" value="<?php echo _('Submit'); ?>">
+				<input type="submit" name="gensubmit" id="gensubmit" class="button" value="<?php echo _('Submit'); ?>">
 			</form>
 		</article>
 
