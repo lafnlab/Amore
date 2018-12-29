4874d41862807666d7d7a87295353f8e937adf17
lafnlab <barista@gottahavacuppamocha.com>
2018-12-29 10:52:25 -0500

    Added configuration table and renamed other tables.

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,97 +1,103 @@
 --
--- Table structure for table `adm`
+-- Table structure for table `configuration`
 --
 
-CREATE TABLE `adm` (
-  `adm_id` varchar(10) NOT NULL,
-  `adm_level` tinytext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for admin/user levels';
+CREATE TABLE `configuration` (
+  `website_url` tinytext NOT NULL,
+  `website_name` tinytext NOT NULL,
+  `website_description` text NOT NULL,
+  `default_locale` tinytext NOT NULL DEFAULT 'en-US',
+  `open_registrations` BOOLEAN DEFAULT 0 COMMENT 'default is closed',
+  `admin_account` tinytext NOT NULL DEFAULT 'admin' COMMENT 'will be first account created',
+  `admin_email` tinytext NOT NULL,
+  `posts_are_called` tinttext NOT NULL DEFAULT 'posts',
+  `post_is_called` tinytext NOT NULL DEFAULT 'post',
+  `reposts_are_called` tinytext NOT NULL DEFAULT 'reposts',
+  `repost_is_called` tinytext NOT NULL DEFAULT 'repost',
+  `users_are_called` tinytext NOT NULL DEFAULT 'users',
+  `user_is_called` tinytext NOT NULL DEFAULT 'user',
+  `favorites_are_called` tinytext NOT NULL DEFAULT 'favorites',
+  `favorite_is_called` tinytext NOT NULL DEFAULT 'favorite',
+  `max_post_length` smallint NOT NULL DEFAULT 500,
+  `banned_user_names` text NOT NULL,
+  `list_with_the_federation_info` BOOLEAN NOT NULL DEFAULT 1 COMMENT 'default is yes',
+  `list_with_fediverse_network` BOOLEAN NOT NULL DEFAULT 1 COMMENT 'default is yes'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for website configuration';
 
 --
--- Dumping data for table `adm`
+-- Table structure for table `currencies`
 --
 
-INSERT INTO `adm` (`adm_id`, `adm_level`) VALUES
-('sеÔÞLÉзBТт', 'USER'),
-('ÛГUojЭПEЯÉ', 'MODERATOR'),
-('ДÖÍsöÊкÔnц', 'TRANSLATOR'),
-('ЗиóВéèàwVO', 'ADMINISTRATOR'),
-('ЮêlùсdzЕХР', 'GUIDE');
-
---
--- Table structure for table `din`
---
-
-CREATE TABLE `din` (
-  `din_id` varchar(10) NOT NULL,
-  `din_name` tinytext NOT NULL,
-  `din_iso` tinytext NOT NULL,
-  `din_symbol` varchar(5) NOT NULL DEFAULT '¤',
-  `din_digital` tinyint(1) NOT NULL
+CREATE TABLE `currencies` (
+  `currencies_id` varchar(10) NOT NULL,
+  `currencies_name` tinytext NOT NULL,
+  `currencies_iso` tinytext NOT NULL,
+  `currencies_symbol` varchar(5) NOT NULL DEFAULT '¤',
+  `currencies_digital` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for currencies/money';
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `eye`
+-- Table structure for table `eye_colors`
 --
 
-CREATE TABLE `eye` (
-  `eye_id` varchar(10) NOT NULL,
-  `eye_color` tinytext NOT NULL
+CREATE TABLE `eye_colors` (
+  `eye_colors_id` varchar(10) NOT NULL,
+  `eye_colors_color` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for eye colors';
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `gen`
+-- Table structure for table `genders`
 --
 
-CREATE TABLE `gen` (
-  `gen_id` varchar(10) NOT NULL,
-  `gen_name` tinytext NOT NULL
+CREATE TABLE `genders` (
+  `genders_id` varchar(10) NOT NULL,
+  `genders_name` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for genders';
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `har`
+-- Table structure for table `hair_colors`
 --
 
-CREATE TABLE `har` (
-  `har_id` varchar(10) NOT NULL,
-  `har_color` tinytext NOT NULL
+CREATE TABLE `hair_colors` (
+  `hair_colors_id` varchar(10) NOT NULL,
+  `hair_colors_color` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for hair colors';
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `i18`
+-- Table structure for table `locales`
 --
 
-CREATE TABLE `i18` (
-  `i18_id` varchar(10) NOT NULL,
-  `i18_language` tinytext NOT NULL,
-  `i18_country` tinytext NOT NULL
+CREATE TABLE `locales` (
+  `locales_id` varchar(10) NOT NULL,
+  `locales_language` tinytext NOT NULL,
+  `locales_country` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for locales';
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `loc`
+-- Table structure for table `locations`
 --
 
-CREATE TABLE `loc` (
-  `loc_id` varchar(10) NOT NULL,
-  `loc_name` tinytext NOT NULL,
-  `loc_parent` varchar(50) NOT NULL
+CREATE TABLE `locations` (
+  `locations_id` varchar(10) NOT NULL,
+  `locations_name` tinytext NOT NULL,
+  `locations_parent` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for locations/places';
 
 --
--- Dumping data for table `loc`
+-- Dumping data for table `locations`
 --
 
-INSERT INTO `loc` (`loc_id`, `loc_name`, `loc_parent`) VALUES
+INSERT INTO `locations` (`locations_id`, `locations_name`, `locations_parent`) VALUES
 ('0000000000', 'WORLD', ''),
 ('0000000001', 'AFRICA', '0000000000'),
 ('0000000002', 'ANTARCTICA', '0000000000'),
@@ -120,49 +126,30 @@ CREATE TABLE `msg` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `nat`
+-- Table structure for table `nationalities`
 --
 
-CREATE TABLE `nat` (
-  `nat_id` varchar(10) NOT NULL,
-  `nat_name` tinytext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for Nationalities';
+CREATE TABLE `nationalities` (
+  `nationalities_id` varchar(10) NOT NULL,
+  `nationalities_name` tinytext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for nationalities';
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pro`
+-- Table structure for table `privacy_levels`
 --
 
-CREATE TABLE `pro` (
-  `pro_id` varchar(10) NOT NULL COMMENT 'same as usr_id',
-  `pro_gen` varchar(10) NOT NULL COMMENT 'from gen table',
-  `pro_sxu` varchar(10) NOT NULL COMMENT 'from sxu table',
-  `pro_eye` varchar(10) NOT NULL COMMENT 'from eye table',
-  `pro_har` varchar(10) NOT NULL COMMENT 'from har table',
-  `pro_loc` varchar(10) NOT NULL COMMENT 'from loc table',
-  `pro_nat` varchar(10) NOT NULL COMMENT 'from nat table',
-  `pro_i18` varchar(10) NOT NULL COMMENT 'from i18 table',
-  `pro_spk` varchar(60) NOT NULL COMMENT 'from spk table',
-  `pro_tzt` varchar(10) NOT NULL COMMENT 'from tzt table'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for user profiles';
-
--- --------------------------------------------------------
-
---
--- Table structure for table `prv`
---
-
-CREATE TABLE `prv` (
-  `prv_id` varchar(10) NOT NULL,
-  `prv_name` tinytext NOT NULL
+CREATE TABLE `privacy_levels` (
+  `privacy_levels_id` varchar(10) NOT NULL,
+  `privacy_levels_name` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for privacy levels';
 
 --
--- Dumping data for table `prv`
+-- Dumping data for table `privacy_levels`
 --
 
-INSERT INTO `prv` (`prv_id`, `prv_name`) VALUES
+INSERT INTO `privacy_levels` (`privacy_levels_id`, `privacy_levels_name`) VALUES
 ('РЖFÂå1ÔÏúL', 'INSTANCE'),
 ('6ьötХ5áзÚZ', 'EVERYONE'),
 ('ñToùòхаþOЪ', 'SELF'),
@@ -174,121 +161,161 @@ INSERT INTO `prv` (`prv_id`, `prv_name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `pst`
+-- Table structure for table `posts`
 --
 
-CREATE TABLE `pst` (
-  `pst_id` varchar(10) NOT NULL,
-  `pst_by` varchar(10) NOT NULL,
-  `pst_timestamp` datetime NOT NULL,
-  `pst_text` text NOT NULL,
-  `pst_lang` varchar(10) NOT NULL,
-  `pst_priv` varchar(10) NOT NULL DEFAULT '6ьötХ5áзÚZ' COMMENT 'default privacy level is for Everyone'
+CREATE TABLE `posts` (
+  `posts_id` varchar(10) NOT NULL,
+  `posts_by` varchar(10) NOT NULL,
+  `posts_timestamp` datetime NOT NULL,
+  `posts_text` text NOT NULL,
+  `posts_language` varchar(10) NOT NULL,
+  `posts_privacy_level` varchar(10) NOT NULL DEFAULT '6ьötХ5áзÚZ' COMMENT 'default privacy level is for Everyone'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for posts';
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `spk`
+-- Table structure for table `spoken_languages`
 --
 
-CREATE TABLE `spk` (
-  `spk_id` varchar(10) NOT NULL,
-  `spk_name` tinytext NOT NULL
+CREATE TABLE `spoken_languages` (
+  `spoken_languages_id` varchar(10) NOT NULL,
+  `spoken_languages_name` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for spoken languages';
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sxu`
+-- Table structure for table `sexualities`
 --
 
-CREATE TABLE `sxu` (
-  `sxu_id` varchar(10) NOT NULL,
-  `sxu_name` tinytext NOT NULL
+CREATE TABLE `sexualities` (
+  `sexualities_id` varchar(10) NOT NULL,
+  `sexualities_name` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for sexualities';
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `tzt`
+-- Table structure for table `time_zones`
 --
 
-CREATE TABLE `tzt` (
-  `tzt_id` varchar(10) NOT NULL,
-  `tzt_abbr` tinytext NOT NULL,
-  `tzt_name` tinytext NOT NULL,
-  `tzt_offset` tinytext NOT NULL,
-  `tzt_dst_offset` tinytext NOT NULL
+CREATE TABLE `time_zones` (
+  `time_zones_id` varchar(10) NOT NULL,
+  `time_zones_abbreviation` tinytext NOT NULL,
+  `time_zones_name` tinytext NOT NULL,
+  `time_zones_offset` tinytext NOT NULL,
+  `time_zones_dst_offset` tinytext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for time zones';
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usr`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `usr` (
-  `usr_id` varchar(10) NOT NULL,
-  `usr_name` tinytext NOT NULL,
-  `usr_pass` tinytext NOT NULL,
-  `usr_email` tinytext NOT NULL,
-  `usr_dob` date NOT NULL,
-  `usr_outbox` mediumtext NOT NULL COMMENT 'collection of messages by the user',
-  `usr_inbox` mediumtext NOT NULL COMMENT 'collection of messages to the user',
-  `usr_liked` mediumtext NOT NULL COMMENT 'collection of items the user likes',
-  `usr_follows` mediumtext NOT NULL COMMENT 'collection of accts the user follows',
-  `usr_followers` mediumtext NOT NULL COMMENT 'collection of accts that follow the user',
-  `usr_created` datetime NOT NULL,
-  `usr_last_login` datetime NOT NULL
+CREATE TABLE `users` (
+  `user_id` varchar(10) NOT NULL,
+  `user_name` tinytext NOT NULL,
+  `user_pass` tinytext NOT NULL,
+  `user_email` tinytext NOT NULL,
+  `user_date_of_birth` date NOT NULL,
+  `user_outbox` mediumtext NOT NULL COMMENT 'collection of messages by the user',
+  `user_inbox` mediumtext NOT NULL COMMENT 'collection of messages to the user',
+  `user_liked` mediumtext NOT NULL COMMENT 'collection of items the user likes',
+  `user_follows` mediumtext NOT NULL COMMENT 'collection of accts the user follows',
+  `user_followers` mediumtext NOT NULL COMMENT 'collection of accts that follow the user',
+  `user_created` datetime NOT NULL,
+  `user_last_login` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for user accounts';
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_levels`
+--
+
+CREATE TABLE `user_levels` (
+  `user_levels_id` varchar(10) NOT NULL,
+  `user_levels_level` tinytext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for admin/user levels';
+
+--
+-- Dumping data for table `user_levels`
+--
+
+INSERT INTO `user_levels` (`user_levels_id`, `user_levels_level`) VALUES
+('sеÔÞLÉзBТт', 'USER'),
+('ÛГUojЭПEЯÉ', 'MODERATOR'),
+('ДÖÍsöÊкÔnц', 'TRANSLATOR'),
+('ЗиóВéèàwVO', 'ADMINISTRATOR'),
+('ЮêlùсdzЕХР', 'GUIDE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_profiles`
+--
+
+CREATE TABLE `user_profiles` (
+  `user_profiles_id` varchar(10) NOT NULL COMMENT 'same as user_id',
+  `user_profiles_gender` varchar(10) NOT NULL COMMENT 'from genders table',
+  `user_profiles_sexuality` varchar(10) NOT NULL COMMENT 'from sexualities table',
+  `user_profiles_eye_color` varchar(10) NOT NULL COMMENT 'from eye_colors table',
+  `user_profiles_hair_color` varchar(10) NOT NULL COMMENT 'from hair_colors table',
+  `user_profiles_location` varchar(10) NOT NULL COMMENT 'from locations table',
+  `user_profiles_nationality` varchar(10) NOT NULL COMMENT 'from nationalities table',
+  `user_profiles_locale` varchar(10) NOT NULL COMMENT 'from locales table',
+  `user_profiles_spoken_language` varchar(60) NOT NULL COMMENT 'from spoken_languages table',
+  `user_profiles_time_zone` varchar(10) NOT NULL COMMENT 'from time_zones table',
+  `user_profiles_description` tinytext NOT NULL COMMENT 'small bio of user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Table for user profiles';
+
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `adm`
+-- Indexes for table `currencies`
 --
-ALTER TABLE `adm`
-  ADD PRIMARY KEY (`adm_id`),
-  ADD UNIQUE KEY `adm_level` (`adm_level`(50));
+ALTER TABLE `currencies`
+  ADD PRIMARY KEY (`currencies_id`);
+  ADD UNIQUE KEY `currencies_iso` (`currencies_iso`(5));
 
 --
--- Indexes for table `din`
+-- Indexes for table `eye_colors`
 --
-ALTER TABLE `din`
-  ADD PRIMARY KEY (`din_id`);
+ALTER TABLE `eye_colors`
+  ADD PRIMARY KEY (`eye_colors_id`);
+  ADD UNIQUE KEY `eye_colors_color` (`eye_colors_color`(25));
 
 --
--- Indexes for table `eye`
+-- Indexes for table `genders`
 --
-ALTER TABLE `eye`
-  ADD PRIMARY KEY (`eye_id`);
+ALTER TABLE `genders`
+  ADD PRIMARY KEY (`genders_id`);
+  ADD UNIQUE KEY `genders_name` (`genders_name`(25));
 
 --
--- Indexes for table `gen`
+-- Indexes for table `hair_colors`
 --
-ALTER TABLE `gen`
-  ADD PRIMARY KEY (`gen_id`);
+ALTER TABLE `hair_colors`
+  ADD PRIMARY KEY (`hair_colors_id`);
+  ADD UNIQUE KEY `hair_colors_color` (`hair_colors_color`(25));
 
 --
--- Indexes for table `har`
+-- Indexes for table `locales`
 --
-ALTER TABLE `har`
-  ADD PRIMARY KEY (`har_id`);
+ALTER TABLE `locales`
+  ADD PRIMARY KEY (`locales_id`);
 
 --
--- Indexes for table `i18`
+-- Indexes for table `locations`
 --
-ALTER TABLE `i18`
-  ADD PRIMARY KEY (`i18_id`);
-
---
--- Indexes for table `loc`
---
-ALTER TABLE `loc`
-  ADD PRIMARY KEY (`loc_id`);
+ALTER TABLE `locations`
+  ADD PRIMARY KEY (`locations`);
 
 --
 -- Indexes for table `msg`
@@ -297,52 +324,63 @@ ALTER TABLE `msg`
   ADD PRIMARY KEY (`msg_id`);
 
 --
--- Indexes for table `nat`
+-- Indexes for table `nationalities`
 --
-ALTER TABLE `nat`
-  ADD PRIMARY KEY (`nat_id`);
+ALTER TABLE `nationalities`
+  ADD PRIMARY KEY (`nationalities_id`);
+    ADD UNIQUE KEY `nationalities_name` (`nationalities_name`(25));
 
 --
--- Indexes for table `pro`
+-- Indexes for table `privacy_levels`
 --
-ALTER TABLE `pro`
-  ADD PRIMARY KEY (`pro_id`);
+ALTER TABLE `privacy_levels`
+  ADD PRIMARY KEY (`privacy_levels_id`),
+  ADD UNIQUE KEY `privacy_levels_name` (`privacy_levels_name`(50));
 
 --
--- Indexes for table `prv`
+-- Indexes for table `posts`
 --
-ALTER TABLE `prv`
-  ADD PRIMARY KEY (`prv_id`),
-  ADD UNIQUE KEY `prv_name` (`prv_name`(50));
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`posts_id`);
 
 --
--- Indexes for table `pst`
+-- Indexes for table `spoken_languages`
 --
-ALTER TABLE `pst`
-  ADD PRIMARY KEY (`pst_id`);
+ALTER TABLE `spoken_languages`
+  ADD PRIMARY KEY (`spoken_languages_id`);
+  ADD UNIQUE KEY `spoken_languages_name` (`spoken_languages_name`(50));
 
 --
--- Indexes for table `spk`
+-- Indexes for table `sexualities`
 --
-ALTER TABLE `spk`
-  ADD PRIMARY KEY (`spk_id`);
+ALTER TABLE `sexualities`
+  ADD PRIMARY KEY (`sexualities_id`);
+  ADD UNIQUE KEY `sexualities_name` (`sexualities_name`(50));
 
 --
--- Indexes for table `sxu`
+-- Indexes for table `time_zones`
 --
-ALTER TABLE `sxu`
-  ADD PRIMARY KEY (`sxu_id`);
+ALTER TABLE `time_zones`
+  ADD PRIMARY KEY (`time_zones_id`);
 
 --
--- Indexes for table `tzt`
+-- Indexes for table `users`
 --
-ALTER TABLE `tzt`
-  ADD PRIMARY KEY (`tzt_id`);
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `user_name` (`user_name`(50));
 
 --
--- Indexes for table `usr`
+-- Indexes for table `user_levels`
 --
-ALTER TABLE `usr`
-  ADD PRIMARY KEY (`usr_id`),
-  ADD UNIQUE KEY `usr_name` (`usr_name`(50));
+ALTER TABLE `user_levels`
+  ADD PRIMARY KEY (`user_levels_id`),
+  ADD UNIQUE KEY `user_levels_level` (`user_levels_level`(50));
+
+--
+-- Indexes for table `user_profiles`
+--
+ALTER TABLE `user_profiles`
+  ADD PRIMARY KEY (`user_profiles_id`);
+
 COMMIT;
