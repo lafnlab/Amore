df59bf46fd07894b2457e174abb3cc849484bf00
lafnlab <barista@gottahavacuppamocha.com>
2018-12-31 11:05:08 -0500

    Added comment to conn.php

--- conn.php
+++ conn.php
@@ -1,5 +1,14 @@
 <?php
-define("DBHOST","");
+/*
+ * conn.php
+ *
+ * Stores information that allows Amore to connect to the database.
+ *
+ * since Amore version 0.1
+ *
+ */
+
+define("DBHOST","localhost"); // Don't change this unless you know what you're doing.
 define("DBNAME","");
 define("DBUSER","");
 define("DBPASS","");
