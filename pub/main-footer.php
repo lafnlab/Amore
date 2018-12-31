ed55f69dd28f15ea9d3b4b3b8c86dd77b9b15b87
lafnlab <barista@gottahavacuppamocha.com>
2018-12-31 14:02:50 -0500

    Added comment to main-footer.php

--- pub/main-footer.php
+++ pub/main-footer.php
@@ -1,3 +1,13 @@
+<?php
+/*
+ * pub/main-footer.php
+ *
+ * This footer ends the HTML for each public facing webpage in Amore.
+ *
+ * since Amore version 0.1
+ *
+ */
+?>
 		<span>Powered by <a href="https://github.com/lafnlab/Amore">Amore</a> v0.1</span>
 	</main>
 </body>
