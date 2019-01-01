48b52f60127038bb0cb8d6bbbe44ec4ea4a4f008
lafnlab <barista@gottahavacuppamocha.com>
2019-01-01 09:00:36 -0500

    Added config query

--- pub/the-post.php
+++ pub/the-post.php
@@ -8,7 +8,7 @@
  */
 
 include_once	"../conn.php";
-#include_once	"../config.php";
+#include_once	"../config.php"; // use the configuration table instead
 include			"../functions.php";
 
 if (isset($_GET["pid"])) {
@@ -19,6 +19,26 @@ if (isset($_GET["pid"])) {
 
 $dbconn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
 mysqli_set_charset($dbconn, "utf8");
+
+// let's get the configuration data
+
+$mysiteq = "SELECT * FROM configuration WHERE primary_key='".SITEKEY."'";
+$mysitequery = mysqli_query($dbconn,$mysiteq);
+while ($mysiteopt = mysqli_fetch_assoc($mysitequery)) {
+	$website_url				= $mysiteopt['website_url'];
+	$website_name				= $mysiteopt['website_name'];
+	$website_description		= $mysiteopt['website_description'];
+	$default_locale			= $mysiteopt['default_locale'];
+	$open_registration		= $mysiteopt['open_registrations'];
+	$posts_are_called			= $mysiteopt['posts_are_called'];
+	$post_is_called			= $mysiteopt['post_is_called'];
+	$reposts_are_called		= $mysiteopt['reposts_are_called'];
+	$repost_is_called			= $mysiteopt['repost_is_called'];
+	$users_are_called			= $mysiteopt['users_are_called'];
+	$user_is_called			= $mysiteopt['user_is_called'];
+	$favorites_are_called	= $mysiteopt['favorites_are_called'];
+	$favorite_is_called		= $mysiteopt['favorite_is_called'];
+}
 
 if ($sel_id != '') {
 
