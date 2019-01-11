4a1d10012ea1d4cda8cb4689d9b5162c4c6e94d9
lafnlab <barista@gottahavacuppamocha.com>
2019-01-11 13:13:38 -0500

    Added more delete-*.php files

---
+++ pub/dash/delete-time-zone.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * pub/dash/delete-time-zone.php
+ *
+ * Delete a time zone from the database.
+ *
+ * since Amore version 0.2
+ *
+ */
+
+include_once	"../../conn.php";
+#include_once	"../config.php";
+include			"../../functions.php";
+
+if (isset($_GET["tzid"])) {
+	$sel_id = $_GET["tzid"];
+} else {
+	$sel_id = "";
+}
+
+$dbconn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+mysqli_set_charset($dbconn, "utf8");
+
+// let's get the configuration data
+
+$mysiteq = "SELECT * FROM configuration WHERE primary_key='".SITEKEY."'";
+$mysitequery = mysqli_query($dbconn,$mysiteq);
+while ($mysiteopt = mysqli_fetch_assoc($mysitequery)) {
+	$website_url				= $mysiteopt['website_url'];
+	$website_name				= $mysiteopt['website_name'];
+	$website_description		= $mysiteopt['website_description'];
+	$default_locale			= $mysiteopt['default_locale'];
+	$open_registration		= $mysiteopt['open_registrations'];
+	$posts_are_called			= $mysiteopt['posts_are_called'];
+	$post_is_called			= $mysiteopt['post_is_called'];
+	$reposts_are_called		= $mysiteopt['reposts_are_called'];
+	$repost_is_called			= $mysiteopt['repost_is_called'];
+	$users_are_called			= $mysiteopt['users_are_called'];
+	$user_is_called			= $mysiteopt['user_is_called'];
+	$favorites_are_called	= $mysiteopt['favorites_are_called'];
+	$favorite_is_called		= $mysiteopt['favorite_is_called'];
+	$max_post_length			= $mysiteopt['max_post_length'];
+}
+
+if ($sel_id != '') {
+	$tzdelq = "DELETE FROM time_zones WHERE time_zones_id='".$sel_id."'";
+	$tzdelquery = mysqli_query($dbconn,$tzdelq);
+	redirect("list-time-zones.php");
+}
+?>
