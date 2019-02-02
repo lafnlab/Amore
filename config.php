b10510d7a05317a6c481915987354e13bc43ef5a
lafnlab <barista@gottahavacuppamocha.com>
2019-02-02 16:39:05 -0500

    I'm still undecided on the fate of config.php

--- config.php
+++ config.php
@@ -2,18 +2,17 @@
 /*
  * config.php
  *
- * This page is deprecated as of Amore version 0.2.
- * Use the configuration table instead.
- * The file will be removed as of Amore version 0.3.
+ * While this was deprecated in Amore v0.2, it now serves with backstop
+ * options and arrays for translation.
  *
  * since Amore version 0.1
  *
  */
 
 #
-# $metalang is the default language. Since i18n isn't setup yet, this default
-# to English unless the site admin changes it.
-$metalang = "en";
+# $metalang is the default language. The default is American English
+# unless the administrator changes it in the configuration table
+$metalang = "en_US";
 
 #
 # $sitetitle is the title of the whole site.
@@ -30,18 +29,19 @@ $siteurl = "https://www.example.com";
 #
 # $metadescription is a summary of the website that may appear on websites
 # See https://moz.com/learn/seo/meta-description
+# This will also serve as the Amore description on the main page.
 $metadescription = _("Amore is open-source PHP/MySQL software for the Fediverse - a decentralized social network of thousands of different communities.");
 
 #
 # $greeting is a generic greeting for any visitor to the site.
 # If a user is logged in, the greeting will change to one appropriate for them.
 # 'Hello' is the default, but can be changed to whatever you prefer.
-$greeting = "Hello";
+$greeting = _("Hello <a href=\"the-login.php\">Guest</a>");
 
 #
 # $visitortitle is a generic term for a visitor who isn't logged in.
 # 'Guest' is default, but can be changed to anything.
-$visitortitle = "Guest";
+# $visitortitle = "Guest";
 
 #
 # $posts_are_called allows the admin to change the name of the posts on their site.
