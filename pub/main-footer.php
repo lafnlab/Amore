96aaa66c6a8ec4fbb37169e231dc452dee5eb582
lafnlab <barista@gottahavacuppamocha.com>
2019-02-08 08:08:39 -0500

    Correcting version number

--- pub/main-footer.php
+++ pub/main-footer.php
@@ -11,7 +11,7 @@
 		</div> <!-- end The Grid -->
 	</main> <!-- end The Container -->
 	<footer class="w3-container w3-large w3-theme-d1">
-		<span><?php echo _("Powered by "); ?><a href="https://github.com/lafnlab/Amore">Amore</a> v0.3-alpha</span>
+		<span><?php echo _("Powered by "); ?><a href="https://github.com/lafnlab/Amore">Amore</a> v0.2.2-alpha</span>
 	</footer>
 </body>
 </html>
