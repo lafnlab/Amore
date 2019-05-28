f691bd6caf559d51c9dcc080e951b376c2ed8378
10th Street Media <10sm@10thstreet.media>
2019-05-28 07:44:09 -0400

    Moved to admin folder and changed dash-nav

--- pub/dash/dash-nav.php
+++ pub/dash/dash-nav.php
@@ -19,8 +19,7 @@
 					<ul>
 						<li><a href="#" title="<?php echo _("Home will show posts from everyone the user is following, including their own."); ?>"><?php echo _("Home"); ?></a></li>
 						<li><a href="#" title="<?php echo _("Mentions will show any posts where the user is mentioned."); ?>"><?php echo _("Mentions"); ?></a></li>
-						<li><a href="#" title="<?php echo _("Messages will be private/direct messages between friends."); ?>"><?php echo _("Messages"); ?></a></li>
-						<li><a href="#" title="<?php echo _("A list of the user's friends (Friends follow each other)."); ?>"><?php echo _("Friends"); ?></a></li>
+						<li><a href="#" title="<?php echo _("Messages will be private/direct messages."); ?>"><?php echo _("Messages"); ?></a></li>
 						<li><a href="#" title="<?php echo _("A list of accounts the user follows."); ?>"><?php echo _("Following"); ?></a></li>
 						<li><a href="#" title="<?php echo _("A list of accounts that follow this user."); ?>"><?php echo _("Followers"); ?></a></li>
 						<li><a href="#" title="<?php echo _("Lists created by the user."); ?>"><?php echo _("Lists"); ?></a></li>
@@ -36,19 +35,36 @@ if(isset($_COOKIE['id'])) {
 		$level	= $idopt['user_level'];
 		if ($level === 'ЗиóВéèàwVO') {
 			echo "\t\t\t\t<div class=\"w3-card-2 w3-theme-l3 w3-padding w3-margin-top\">\n";
+			echo "\t\t\t\t\t<h5>"._('Users')."</h5>\n";
 			echo "\t\t\t\t\t<ul>\n";
-			echo "\t\t\t\t\t\t<li><a href=\"list-currencies.php\">"._('List currencies')."</a></li>\n";
-			echo "\t\t\t\t\t\t<li><a href=\"list-eye-colors.php\">"._('List eye colors')."</a></li>\n";
-			echo "\t\t\t\t\t\t<li><a href=\"list-genders.php\">"._('List genders')."</a></li>\n";
-			echo "\t\t\t\t\t\t<li><a href=\"list-hair-colors.php\">"._('List hair colors')."</a></li>\n";
-			echo "\t\t\t\t\t\t<li><a href=\"list-locales.php\">"._('List locales')."</a></li>\n";
-			echo "\t\t\t\t\t\t<li><a href=\"list-nationalities.php\">"._('List nationalities')."</a></li>\n";
-			echo "\t\t\t\t\t\t<li><a href=\"list-places.php\">"._('List places')."</a></li>\n";
-			echo "\t\t\t\t\t\t<li><a href=\"list-relationship-statuses.php\">"._('List relationship statuses')."</a></li>\n";
-			echo "\t\t\t\t\t\t<li><a href=\"list-sexualities.php\">"._('List sexualities')."</a></li>\n";
-			echo "\t\t\t\t\t\t<li><a href=\"list-spoken-languages.php\">"._('List spoken languages')."</a></li>\n";
-			echo "\t\t\t\t\t\t<li><a href=\"list-time-zones.php\">"._('List time zones')."</a></li>\n";
-			echo "\t\t\t\t\t\t<li><a href=\"list-users.php\">"._('List users')."</a></li>\n";
+			echo "\t\t\t\t\t\t<li><a href=\"list-users.php\">"._('All users')."</a></li>\n";
+			echo "\t\t\t\t\t\t<li><a href=\"add-user.php\">"._('Add a user')."</a></li>\n";
+			echo "\t\t\t\t\t\t<li><a href=\"my-profile.php\">"._('Your profile')."</a></li>\n";
+			echo "\t\t\t\t\t</ul>\n";
+			echo "\t\t\t\t\t<h5>"._('Settings')."</h5>\n";
+			echo "\t\t\t\t\t<ul>\n";
+			echo "\t\t\t\t\t\t<li>"._('General')."\n";
+			echo "\t\t\t\t\t\t\t<ul>\n";
+			echo "\t\t\t\t\t\t\t\t<li><a href=\"admin/settings-configure.php\">"._('Configuration')."</a></li>\n";
+			echo "\t\t\t\t\t\t\t\t<li><a href=\"admin/settings-home-page.php\">"._('Home page')."</a></li>\n";
+			echo "\t\t\t\t\t\t\t\t<li><a href=\"admin/settings-privacy.php\">"._('Privacy')."</a></li>\n";
+			echo "\t\t\t\t\t\t\t</ul>\n";
+			echo "\t\t\t\t\t\t</li>\n";
+			echo "\t\t\t\t\t\t<li>"._('Metadata')."\n";
+			echo "\t\t\t\t\t\t\t<ul>\n";
+			echo "\t\t\t\t\t\t\t\t<li><a href=\"admin/list-currencies.php\">"._('Currencies')."</a></li>\n";
+			echo "\t\t\t\t\t\t\t\t<li><a href=\"admin/list-eye-colors.php\">"._('Eye colors')."</a></li>\n";
+			echo "\t\t\t\t\t\t\t\t<li><a href=\"admin/list-genders.php\">"._('Genders')."</a></li>\n";
+			echo "\t\t\t\t\t\t\t\t<li><a href=\"admin/list-hair-colors.php\">"._('Hair colors')."</a></li>\n";
+			echo "\t\t\t\t\t\t\t\t<li><a href=\"admin/list-locales.php\">"._('Locales')."</a></li>\n";
+			echo "\t\t\t\t\t\t\t\t<li><a href=\"admin/list-nationalities.php\">"._('Nationalities')."</a></li>\n";
+			echo "\t\t\t\t\t\t\t\t<li><a href=\"admin/list-places.php\">"._('Places')."</a></li>\n";
+			echo "\t\t\t\t\t\t\t\t<li><a href=\"admin/list-relationship-statuses.php\">"._('Relationship statuses')."</a></li>\n";
+			echo "\t\t\t\t\t\t\t\t<li><a href=\"admin/list-sexualities.php\">"._('Sexualities')."</a></li>\n";
+			echo "\t\t\t\t\t\t\t\t<li><a href=\"admin/list-spoken-languages.php\">"._('Spoken languages')."</a></li>\n";
+			echo "\t\t\t\t\t\t\t\t<li><a href=\"admin/list-time-zones.php\">"._('Time zones')."</a></li>\n";
+			echo "\t\t\t\t\t\t\t</ul>\n";
+			echo "\t\t\t\t\t\t</li>\n";
 			echo "\t\t\t\t\t</ul>\n";
 			echo "\t\t\t\t</div>\n";
 		}
