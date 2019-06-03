8d7cfcbe7950300eaef70951afbefbc9512509cc
10th Street Media <10sm@10thstreet.media>
2019-06-03 12:02:35 -0400

    Fixed typo

--- pub/dash/admin/list-time-zones.php
+++ pub/dash/admin/list-time-zones.php
@@ -25,6 +25,12 @@ include_once "admin-nav.php";
 			<div class="w3-card-2 w3-theme-l3 w3-padding">
 			<h4><?php echo _("List of time zones"); ?></h4>
 				<table>
+					<tr>
+						<th><?php echo _("Time zone name"); ?></th>
+						<th><?php echo _("Offset"); ?></th>
+						<th><?php echo _("DST offset"); ?></th>
+						<th colspan="2"><?php echo _("Actions"); ?></th>
+					</tr>
 <?php
 		$tztq = "SELECT * FROM time_zones ORDER BY time_zone_name ASC";
 		$tztquery = mysqli_query($dbconn,$tztq);
@@ -33,7 +39,7 @@ include_once "admin-nav.php";
 			$tzt_name	= $tztopt['time_zone_name'];
 			$tzt_id		= $tztopt['time_zone_id'];
 			$tzt_off		= $tztopt['time_zone_offset'];
-			$tzt_dst		= $tztopt['time_zone_dst_offset'];
+			$tzt_dst		= $tztopt['time_zone_DST_offset'];
 			echo "\t\t\t\t\t<tr>\n";
 			echo "\t\t\t\t\t\t<td><a href=\"the-time-zone.php?tzid=".$tzt_id."\">"._($tzt_name)."</a></td>\n";
 			echo "\t\t\t\t\t\t<td>".$tzt_off."</td>\n";
